29fa1a98dc9fef5a6a15215a0d1aa25077c3119e
Joa0DeL1ma <161715327+Joa0DeL1ma@users.noreply.github.com>
2024-07-19 16:08:11 -0300

    Started

---
+++ app/src/main/java/com/example/bestof/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.bestof
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.bestof.ui.theme.BestOfTheme
+import com.example.bestof.ui.theme.josefinFontFamily
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BestOfTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Header()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Header() {
+    Row (Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        Alignment.CenterVertically
+        ) {
+        IconButton(onClick = { /* ação ao clicar no botão */ }) {
+            Icon(Icons.Filled.Menu, contentDescription = "Options Icon") }
+        Text(text = "Best of", fontFamily = josefinFontFamily, fontSize = 24.sp)
+        IconButton(onClick = { /* ação ao clicar no botão */ }) {
+            Icon(Icons.Filled.Search, contentDescription = "Search Icon")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeaderPreview() {
+    BestOfTheme {
+        Header()
+    }
+}
